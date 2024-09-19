977332abbfe3890f29063d3624debb9e7c084fec
zelt <79714781+ghoulatsch@users.noreply.github.com>
2024-09-19 23:17:20 +0200

    Add files via upload

---
+++ NoFlashingTabs.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         No flashing tabs
+// @namespace    https://stackoverflow.com/questions/36740937/how-do-i-prevent-a-page-and-tabs-document-title-from-changing
+// @homepage     https://stackoverflow.com/questions/36740937/how-do-i-prevent-a-page-and-tabs-document-title-from-changing
+// @version      1.0
+// @description  Stop tabs from flashing.
+// @author       see namespace
+// @match        *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    Object.defineProperty(document, 'title', {
+        set: function(){}
+    });
+})();
