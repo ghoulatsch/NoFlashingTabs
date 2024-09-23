0c36f7ef0a89ffb8cbb6001ffdf08fad51a9a979
zelt <79714781+ghoulatsch@users.noreply.github.com>
2024-09-23 18:51:47 +0200

    Update NoFlashingTabs.user.js

--- NoFlashingTabs.user.js
+++ NoFlashingTabs.user.js
@@ -2,7 +2,7 @@
 // @name         No Flashing Tabs
 // @namespace    https://stackoverflow.com/questions/36740937/how-do-i-prevent-a-page-and-tabs-document-title-from-changing
 // @homepage     https://github.com/ghoulatsch
-// @version      1.0
+// @version      240923
 // @description  Stop tabs from flashing.
 // @author       see namespace
 // @match        *://*/*
